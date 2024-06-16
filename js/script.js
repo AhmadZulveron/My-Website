1da5b1e70721583f339cff341c4caff1c945dacc
AhmadZulveron <ahmadzvrn8890@gmail.com>
2024-06-16 11:21:01 +0700

    Tambah animasi pada section about

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ humberger.addEventListener('click', function() {
     humberger.classList.toggle('humberger-active');
     navMenu.classList.toggle('hidden');
 })
+
+//Animation slide in-out
+document.addEventListener("DOMContentLoaded", function () {
+    const sections = document.querySelectorAll('.section');
+
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                // entry.target.querySelector('.slide-in-top')
+                // entry.target.querySelector('.slide-in-top').classList.add('slide-in-top');
+
+                entry.target.querySelector('.left-content').classList.add('visible', 'slide-out-left');
+                entry.target.querySelector('.left-content').classList.remove('slide-in-left');
+
+                entry.target.querySelector('.right-content').classList.add('visible', 'slide-out-right');
+                entry.target.querySelector('.right-content').classList.remove('slide-in-right');
+            } else {
+                entry.target.querySelector('.left-content').classList.remove('visible', 'slide-out-left');
+                entry.target.querySelector('.left-content').classList.add('slide-in-left');
+
+                entry.target.querySelector('.right-content').classList.remove('visible', 'slide-out-right');
+                entry.target.querySelector('.right-content').classList.add('slide-in-right');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
